29f55bede1a6e5affc291576d9dc5fff23efa306
Chowdesha123 <chowdeshchowda1764Ggmail.com>
2024-11-26 14:05:27 +0530

    first commit

---
+++ src/product.js
@@ -0,0 +1,81 @@
+import product1 from "./assets/1.png.png";
+import product2 from "./assets/2.png.png";
+import product3 from "./assets/3.png.png";
+import product4 from "./assets/4.png.png";
+import product5 from "./assets/5.png.png";
+import product6 from "./assets/6.png.png";
+import product7 from "./assets/7.png.png";
+import product8 from "./assets/8.png.png";
+import product9 from "./assets/9.png.png";
+ 
+
+export const PRODUCTS=[
+    {
+        id: 1,
+        productName: "Jacket",
+        price: 786.0,
+        productImg: product2,
+
+    },
+    {
+        id: 2,
+        productName: "Camera",
+        price: 689.0,
+        productImg: product3,
+
+    },
+    {
+        id: 3,
+        productName: "IPhone",
+        price: 999.0,
+        productImg: product1,
+    },
+  
+    {
+        id: 4,
+        productName: "Laptop",
+        price: 228.0,
+        productImg: product4,
+
+    },
+    {
+        id: 5,
+        productName: "LEDTV",
+        price: 267.0,
+        productImg: product8,
+
+    },
+    {
+        id: 6,
+        productName: "Gagal",
+        price: 20.0,
+        productImg: product5,
+
+    },
+    
+    {
+        id: 7,
+        productName: "Smart Watch",
+        price: 149.0,
+        productImg: product9,
+
+    },
+    
+    {
+        id: 8,
+        productName: "Shoes",
+        price: 135.0,
+        productImg: product6,
+
+    },
+   
+   
+    {
+        id: 9,
+        productName: "Cap",
+        price: 45.0,
+        productImg: product7,
+
+    },
+
+]
